e5e874e3ebeedba3a00fdf04e8898046c103e4fc
archibird <robindbor@gmail.com>
2023-11-12 00:28:42 -0800

    indentation change

--- lua/myLuaConf/opts.lua
+++ lua/myLuaConf/opts.lua
@@ -26,7 +26,7 @@ vim.o.cpoptions = 'I'
 -- vim.o.tabstop = 4
 -- vim.o.softtabstop = 4
 -- vim.o.shiftwidth = 4
--- vim.o.expandtab = true
+vim.o.expandtab = true
 
 -- stops line wrapping from being confusing
 vim.o.breakindent = true
