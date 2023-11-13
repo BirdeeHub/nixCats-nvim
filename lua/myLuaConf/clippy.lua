6aa5b70033478852c1d1bc7a36ac874b946eb650
archibird <robindbor@gmail.com>
2023-11-12 22:22:02 -0800

    removed a fix that was never required

--- lua/myLuaConf/clippy.lua
+++ lua/myLuaConf/clippy.lua
@@ -9,31 +9,7 @@
 
 -- So, meet clippy.lua
 
--- a collection of mappings to allow you to yank to clipboard using <leader>y
--- as well as a few nice paste options, and ctrl+a
--- in normal mode, it accepts motions as well.
-vim.cmd([[
-  function! Yank_to_clipboard(type)
-    silent exec 'normal! `[v`]"+y'
-    silent exec 'let @/=@"'
-  endfunction
-  " nmap <silent> <leader>y :set opfunc=Yank_to_clipboard<CR>g@
-  " vnoremap <silent> <leader>y "+y
-  " xnoremap <silent> <leader>y "+y
-  " nnoremap <silent> <leader>yy "+yy
-  " vnoremap <silent> <leader>yy "+yy
-  " xnoremap <silent> <leader>yy "+yy
-  " nnoremap <silent> <leader>Y "+yy
-  " vnoremap <silent> <leader>Y "+yy 
-  " xnoremap <silent> <leader>Y "+yy
-  " nnoremap <silent> <C-a> gg0vG$
-  " vnoremap <silent> <C-a> gg0vG$
-  " xnoremap <silent> <C-a> gg0vG$
-  " nnoremap <silent> <leader>p "+p
-  " inoremap <silent> <C-p> <C-r>+
-  " xnoremap <silent> <leader>P "_dP
-]])
-vim.keymap.set("n", '<leader>y', [[:set opfunc=Yank_to_clipboard<CR>g@]], { silent = true, desc = 'Yank to clipboard (accepts motions)' })
+vim.keymap.set("n", '<leader>y', '"+y', { noremap = true, silent = true, desc = 'Yank to clipboard' })
 vim.keymap.set({"v", "x"}, '<leader>y', '"+y', { noremap = true, silent = true, desc = 'Yank to clipboard' })
 vim.keymap.set({"n", "v", "x"}, '<leader>yy', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
 vim.keymap.set({"n", "v", "x"}, '<leader>Y', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
@@ -42,3 +18,22 @@ vim.keymap.set('n', '<leader>p', '"+p', { noremap = true, silent = true, desc = 
 vim.keymap.set('i', '<C-p>', '<C-r>+', { noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
 vim.keymap.set("x", "<leader>P", '"_dP', { noremap = true, silent = true, desc = 'Paste over selection without erasing unnamed register' })
 
+
+-- so, my normal mode <leader>y randomly didnt accept any motions.
+-- I didnt know it was meant to, because that was my first install of neovim.
+-- If that ever happens to you, comment out the normal one, then uncomment this keymap and the function below it.
+-- A full purge of all previous config files installed via pacman fixed it for me, as the pacman config was the one that had that problem.
+-- I thought I was cool, but apparently I was doing a workaround to restore default behavior.
+
+
+-- a collection of mappings to allow you to yank to clipboard using <leader>y
+-- as well as a few nice paste options, and ctrl+a
+-- in normal mode, it accepts motions as well.
+-- vim.keymap.set("n", '<leader>y', [[:set opfunc=Yank_to_clipboard<CR>g@]], { silent = true, desc = 'Yank to clipboard (accepts motions)' })
+-- vim.cmd([[
+--   function! Yank_to_clipboard(type)
+--     silent exec 'normal! `[v`]"+y'
+--     silent exec 'let @/=@"'
+--   endfunction
+--   " nmap <silent> <leader>y :set opfunc=Yank_to_clipboard<CR>g@
+-- ]])
