5e7900ed39425698aa57f75771b77d42fb387dc7
BirdeeHub <robindbor@gmail.com>
2023-11-23 11:15:56 -0800

    typos and comments

--- init.lua
+++ init.lua
@@ -4,4 +4,8 @@
 -- when wrapRc setting is false, this file will be executed.
 -- Leave it unchanged, so that you dont have a different config
 -- between unwrapped and wrapped lua configurations.
+
 require(require('nixCats').RCName)
+-- when using wrapRc = false, if you don't set RCName
+-- this obviously won't find the right thing to require
+-- so either set RCName in the flake, or make this require your folder of choice
