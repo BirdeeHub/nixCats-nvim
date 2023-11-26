99b5fc2b977be14cc35415f2ea77a90762609187
BirdeeHub <robindbor@gmail.com>
2023-11-25 17:59:13 -0800

    name conflict with debug lua folder

--- flake.nix
+++ flake.nix
@@ -278,7 +278,7 @@
           custom = true;
           neonixdev = true;
           test = true;
-          debug = true;
+          debug = false;
           # this does not have an associated category of plugins, 
           # but lua can still check for it
           lspDebugMode = false;
