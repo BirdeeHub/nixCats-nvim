aa1b59341731b415324643368fe1cce60e6c23db
BirdeeHub <robindbor@gmail.com>
2023-12-03 05:35:32 -0800

    nix flake update for nixpkgs

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,1 @@
-if ( require('nixCats').colorscheme == [[onedark]] or require('nixCats').colorscheme == [[catppuccin]] ) then
-    vim.cmd([[hi LineNr guifg=#bb9af7]])
-end
+vim.cmd([[hi LineNr guifg=#bb9af7]])
