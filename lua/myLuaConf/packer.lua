cafe49b338ce25aceda0e7b4d1da19ac69fc151a
BirdeeHub <robindbor@gmail.com>
2023-12-30 11:10:03 -0800

    apparently pckr is better

--- lua/myLuaConf/packer.lua
+++ lua/myLuaConf/packer.lua
@@ -1,39 +1,42 @@
 local isNixInstalled = require('myLuaConf.isNixCats')
 if not isNixInstalled then
 
-  local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-      vim.cmd [[packadd packer.nvim]]
-      return true
-    end
-    return false
+local function bootstrap_pckr()
+  local pckr_path = vim.fn.stdpath("data") .. "/pckr/pckr.nvim"
+
+  if not vim.loop.fs_stat(pckr_path) then
+    vim.fn.system({
+      'git',
+      'clone',
+      "--filter=blob:none",
+      'https://github.com/lewis6991/pckr.nvim',
+      pckr_path
+    })
   end
 
-  local packer_bootstrap = ensure_packer()
+  vim.opt.rtp:prepend(pckr_path)
+end
 
-  return require('packer').startup(function(use)
-    use { 'wbthomason/packer.nvim', }
------------------------------------------------
+bootstrap_pckr()
 
-    use { 'joshdick/onedark.vim', }
-    use { 'nvim-tree/nvim-web-devicons', }
+require('pckr').add{
 
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
+    { 'joshdick/onedark.vim', },
+    { 'nvim-tree/nvim-web-devicons', },
+
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
       requires = {
         'nvim-treesitter/nvim-treesitter-textobjects',
       },
-    }
-    use {'nvim-telescope/telescope.nvim',
+    },
+    {'nvim-telescope/telescope.nvim',
       requires = {
         'nvim-lua/plenary.nvim',
         { 'nvim-telescope/telescope-fzf-native.nvim', run = 'which make && make', }
       },
-    }
+    },
 
-    use { 'neovim/nvim-lspconfig',
+    { 'neovim/nvim-lspconfig',
       requires = {
         { 'williamboman/mason.nvim', },
         { 'williamboman/mason-lspconfig.nvim', },
@@ -41,9 +44,9 @@ if not isNixInstalled then
         { 'folke/neodev.nvim', },
         { 'folke/neoconf.nvim', },
       },
-    }
+    },
 
-    use { 'hrsh7th/nvim-cmp',
+    { 'hrsh7th/nvim-cmp',
       requires = {
         { 'onsails/lspkind.nvim', },
         { 'L3MON4D3/LuaSnip', },
@@ -57,42 +60,35 @@ if not isNixInstalled then
         { 'hrsh7th/cmp-cmdline', },
         { 'dmitmel/cmp-cmdline-history', },
       },
-    }
+    },
 
-    use { 'mfussenegger/nvim-dap',
+    { 'mfussenegger/nvim-dap',
       requires = {
         { 'rcarriga/nvim-dap-ui', },
         { 'theHamsta/nvim-dap-virtual-text', },
         { 'jay-babu/mason-nvim-dap.nvim', },
       },
-    }
+    },
 
-    use { 'm-demare/hlargs.nvim', }
-    use { 'mbbill/undotree', }
-    use { 'tpope/vim-fugitive', }
-    use { 'tpope/vim-rhubarb', }
-    use { 'tpope/vim-sleuth', }
-    use { 'folke/which-key.nvim', }
-    use { 'lewis6991/gitsigns.nvim', }
-    use { 'nvim-lualine/lualine.nvim', }
-    use { 'lukas-reineke/indent-blankline.nvim', }
-    use { 'numToStr/Comment.nvim', }
-    use { 'kylechui/nvim-surround',
+    { 'm-demare/hlargs.nvim', },
+    { 'mbbill/undotree', },
+    { 'tpope/vim-fugitive', },
+    { 'tpope/vim-rhubarb', },
+    { 'tpope/vim-sleuth', },
+    { 'folke/which-key.nvim', },
+    { 'lewis6991/gitsigns.nvim', },
+    { 'nvim-lualine/lualine.nvim', },
+    { 'lukas-reineke/indent-blankline.nvim', },
+    { 'numToStr/Comment.nvim', },
+    { 'kylechui/nvim-surround',
       requires = { 'tpope/vim-repeat', },
-    }
-    use({
+    },
+    {
       "iamcco/markdown-preview.nvim",
       run = function() vim.fn["mkdp#util#install"]() end,
-    })
+    },
 
-    -- use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+    -- { "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, },
 
-
-    -----------------------------
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-      require('packer').sync()
-    end
-  end)
+  }
 end
