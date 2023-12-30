abb10ac79dfe798254ec4157e9cdade3b6fd23eb
BirdeeHub <robindbor@gmail.com>
2023-12-30 09:57:36 -0800

    better but still stuff missing

--- lua/myLuaConf/packer.lua
+++ lua/myLuaConf/packer.lua
@@ -79,10 +79,14 @@ if not isNixInstalled then
     use { 'kylechui/nvim-surround',
       requires = { 'tpope/vim-repeat', },
     }
-    use({
-      "iamcco/markdown-preview.nvim",
-      run = function() vim.fn["mkdp#util#install"]() end,
-    })
+    -- use({
+    --   "iamcco/markdown-preview.nvim",
+    --   run = function() vim.fn["mkdp#util#install"]() end,
+    -- })
+
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
+
     -----------------------------
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
