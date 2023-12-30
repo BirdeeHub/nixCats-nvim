997cc5042af0256d822fcc1deba55092845df5e9
BirdeeHub <robindbor@gmail.com>
2023-12-30 13:23:49 -0800

    slightly improved api

--- lua/nixCatsUtils/init.lua
+++ lua/nixCatsUtils/init.lua
@@ -4,14 +4,15 @@ M.isNixCats, _ = pcall(function() require("nixCats") end)
 
 function M.setup(v)
     if not M.isNixCats then
+        local nixCats_pckr_default
         if type(v) == "table" and v.default_cat_value ~= nil then
-            vim.g.nixCats_pckr_default = v.default_cat_value
+            nixCats_pckr_default = v.default_cat_value
         else
-            vim.g.nixCats_pckr_default = true
+            nixCats_pckr_default = true
         end
         package.loaded.nixCats = nil
         -- if not in nix, just make it return a boolean
-        require('_G').nixCats = function(_) return vim.g.nixCats_pckr_default end
+        require('_G').nixCats = function(_) return nixCats_pckr_default end
     end
 end
 
