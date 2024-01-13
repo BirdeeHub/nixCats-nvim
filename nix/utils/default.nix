7e1429958c7131319960f24723dadbbd36295208
BirdeeHub <robindbor@gmail.com>
2024-01-12 22:00:58 -0800

    update

--- nix/utils/default.nix
+++ nix/utils/default.nix
@@ -91,52 +91,7 @@ rec {
             packageDefinitions defaultPackageName;
         } utils);
 
-    templates = {
-      fresh = {
-        path = ../templates/fresh;
-        description = "starting point template for making your neovim flake";
-      };
-      nixosModule = {
-        path = ../templates/nixosModule;
-        description = ''
-          An EXAMPLE nixOS module configuration template
-        '';
-      };
-      homeModule = {
-        path = ../templates/homeManager;
-        description = ''
-          An EXAMPLE Home Manager module configuration template
-        '';
-      };
-      mergeFlakeWithExisting = {
-        path = ../templates/touchUpExisting;
-        description = ''
-          An EXAMPLE template showing how to merge in parts of other nixCats repos.
-        '';
-      };
-      LSPs = {
-        path = ../templates/LSPs;
-        description = ''
-          An EXAMPLE template showing how to merge in parts of other nixCats repos.
-        '';
-      };
-      luaUtils = {
-        path = ../templates/luaUtils;
-        description = ''
-          A template that includes lua utils for using neovim package managers
-          when your config file is not loaded via nix.
-        '';
-      };
-      overlayfile = {
-        path = ../templates/overlayfile;
-        description = ''
-          A template for an empty overlay file defined as described in
-          :help nixCats.flake.nixperts.overlays and overlays/default.nix
-        '';
-      };
-
-      default = utils.templates.fresh;
-    };
+    templates = import ../templates;
 
   };
 
