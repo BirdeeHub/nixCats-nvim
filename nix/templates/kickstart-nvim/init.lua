f43f1537596ceb9156dfe06acab592313b6f062e
BirdeeHub <robindbor@gmail.com>
2024-01-13 02:08:22 -0800

    fix template for branch name change

--- nix/templates/kickstart-nvim/init.lua
+++ nix/templates/kickstart-nvim/init.lua
@@ -120,7 +120,7 @@ require('nixCatsUtils.lazyCat').setup( pluginList, nixLazyPath,
 --[[ Uh-oh! We don't want to use mason on nix! ]]
 --[[ luckily we have our lazyAdd utility!      ]]
 --[[ We can use it to add true only if not     ]]
---[[ loaded via niCats. When NOT loaded in nix ]]
+--[[ loaded via nix. When NOT loaded in nix    ]]
 --[[ It returns the 1st value, otherwise,      ]]
 --[[ it returns the 2nd value.                 ]]
 --[[    (or nil if there wasnt one)            ]]
