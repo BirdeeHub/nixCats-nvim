81d132c54de9d28d7888a6449cd6d9834e2ade45
BirdeeHub <robindbor@gmail.com>
2024-01-21 05:46:53 -0800

    lazy built in overlay so it can keep the same name

--- overlays/customBuildsOverlay.nix
+++ overlays/customBuildsOverlay.nix
@@ -9,7 +9,7 @@ importName: inputs: let
           owner = "folke";
           repo = "lazy.nvim";
           rev = "28126922c9b54e35a192ac415788f202c3944c9f";
-          sha256 = "";
+          sha256 = "sha256-Qicyec1ZvSb5JVVTW8CrTjndHCLso8Rb2V5IA6D4Rps=";
         };
         meta.homepage = "https://github.com/folke/lazy.nvim/";
       };
