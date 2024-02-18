74218d376d89f9ab9c381646a5e2dd90b512fc9d
BirdeeHub <robindbor@gmail.com>
2024-02-18 06:55:40 -0800

    nix flake update and fix to a paste keybind

--- init.lua
+++ init.lua
@@ -250,7 +250,7 @@ vim.keymap.set({"n", "v", "x"}, '<leader>yy', '"+yy', { noremap = true, silent =
 vim.keymap.set({"n", "v", "x"}, '<leader>Y', '"+yy', { noremap = true, silent = true, desc = 'Yank line to clipboard' })
 vim.keymap.set({"n", "v", "x"}, '<C-a>', 'gg0vG$', { noremap = true, silent = true, desc = 'Select all' })
 vim.keymap.set({'n', 'v', 'x'}, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'Paste from clipboard' })
-vim.keymap.set('i', '<C-p>', '<C-r>+', { noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
+vim.keymap.set('i', '<C-p>', '<C-r><C-p>+', { noremap = true, silent = true, desc = 'Paste from clipboard from within insert mode' })
 vim.keymap.set("x", "<leader>P", '"_dP', { noremap = true, silent = true, desc = 'Paste over selection without erasing unnamed register' })
 
 -- so, my normal mode <leader>y randomly didnt accept any motions.
