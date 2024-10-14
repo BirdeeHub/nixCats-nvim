37a9fe17a7752dacde99c0d646157d8039c66864
BirdeeHub <robindbor@gmail.com>
2024-10-14 13:26:57 -0700

    moar comments

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -10,6 +10,11 @@ vim.cmd.colorscheme(colorschemeName)
 -- NOTE: you can check if you included the category with the thing wherever you want.
 if nixCats('general.extra') then
   -- I didnt want to bother with lazy loading this.
+  -- I could put it in opt and put it in a spec anyway
+  -- and then not set any handlers and it would load at startup,
+  -- but why... I guess I could make it load
+  -- after the other lze definitions in the next call using priority value?
+  -- didnt seem necessary.
   vim.g.loaded_netrwPlugin = 1
   require("oil").setup({
     default_file_explorer = true,
