71305b4668b881748f08099f68bc01d65c84a45a
BirdeeHub <robindbor@gmail.com>
2024-10-14 14:28:20 -0700

    inherit > start = start;

--- nix/builder/default.nix
+++ nix/builder/default.nix
@@ -314,8 +314,7 @@ import ./wrapNeovim.nix {
   inherit extraMakeWrapperArgs nixCats preWrapperShellCode customRC;
   inherit (settings) vimAlias viAlias withRuby withPerl extraName withNodeJs aliases gem_path;
   pluginsOG.myVimPackage = {
-    start = start;
-    inherit opt;
+    inherit start opt;
   };
     /* the function you would have passed to python.withPackages */
   # extraPythonPackages = combineCatsOfFuncs extraPythonPackages;
