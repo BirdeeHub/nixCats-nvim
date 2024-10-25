747a75cc7e2a22bd6381afb1615b0a2c53434193
BirdeeHub <robindbor@gmail.com>
2024-10-25 01:49:46 -0700

    comment

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,7 +2,6 @@
   outputs = { ... }: let
     utils = import ../.;
   in {
-    # everything is in utils.
     utils = builtins.trace "Deprecation warning: github:BirdeeHub/nixCats-nvim?dir=nix flake input is being deprecated. Please use github:BirdeeHub/nixCats-nvim instead." utils;
     templates = builtins.trace "Deprecation warning: github:BirdeeHub/nixCats-nvim?dir=nix flake input is being deprecated. Please use github:BirdeeHub/nixCats-nvim instead." utils.templates;
   };
