af9d934864b263ef386e226420a509f5d6f9f786
BirdeeHub <robindbor@gmail.com>
2024-10-27 18:43:12 -0700

    start of working on adding tests

--- tests/flake.nix
+++ tests/flake.nix
@@ -5,20 +5,43 @@
   };
   outputs = { self, nixpkgs, nixCats, ... }@inputs: let
     forAllSys = nixCats.utils.eachSystem nixpkgs.lib.platforms.all;
+    mkTestVim = system: let
+      luaPath = ./.;
+      dependencyOverlays = [
+      ];
+      categoryDefinitions = { pkgs, settings, categories, name, ... }@packageDef: {
+      };
+      packageDefinitions = {
+        testvim = { pkgs, ... }: {
+          settings = {
+          };
+          categories = {
+          };
+        };
+      };
+    in nixCats.utils.baseBuilder luaPath {
+        inherit nixpkgs system dependencyOverlays;
+        extra_pkg_config = {
+          allowUnfree = true;
+        };
+        nixCats_passthru = {};
+      } categoryDefinitions packageDefinitions "testvim";
   in forAllSys (system: let
     pkgs = import nixpkgs { inherit system; };
+    testvim = mkTestVim system;
   in
   {
     checks = {
       default = pkgs.stdenv.mkDerivation {
-        name = "test-1";
-        src = ./.;
+        name = "itbuilds";
+        src = testvim;
         doCheck = true;
         dontUnpack = true;
         buildPhase = ''
           mkdir -p $out
         '';
         checkPhase = ''
+          [ ! -f $src/bin/testvim ] && exit 1 || echo "$src/bin/testvim exists!"
         '';
       };
     };
