9b87b261e324cd7b5bd16308c58d13027eef06c8
BirdeeHub <robindbor@gmail.com>
2024-10-27 18:52:34 -0700

    start of working on adding tests

---
+++ tests/init.lua
@@ -0,0 +1,14 @@
+_G.my_assert = function(c, message)
+    if not c then
+        if nixCats('killAfter') then
+            print("assertion failed: " .. message)
+            vim.cmd.cquit()
+        else
+            error("assertion failed: " .. message)
+        end
+    end
+end
+
+-- Test some stuff here
+
+-- Still deciding if I want to do it this way, use buster, or both
