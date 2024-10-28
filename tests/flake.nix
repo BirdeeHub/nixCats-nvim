6d081f603549bfea148d0e6c50bd07d7dcbb464d
BirdeeHub <robindbor@gmail.com>
2024-10-28 00:16:50 -0700

    start of working on adding tests

--- tests/flake.nix
+++ tests/flake.nix
@@ -33,18 +33,35 @@
   in
   {
     checks = {
-      default = pkgs.stdenv.mkDerivation {
+      default = self.checks.${system}.drv;
+      drv = pkgs.stdenv.mkDerivation {
         name = "itbuilds";
-        src = testvim;
+        src = ./.;
         doCheck = true;
         dontUnpack = true;
         buildPhase = ''
           mkdir -p $out
         '';
-        checkPhase = ''
+        checkPhase = let
+          drvtestvim = testvim.override (prev: {
+            packageDefinitions = prev.packageDefinitions // {
+              ${prev.name} = utils.mergeCatDefs prev.packageDefinitions.${prev.name} ({ pkgs, ... }: {
+                settings = {
+                };
+                categories = {
+                  nix_test_info = {
+                    hello = "world";
+                  };
+                };
+              });
+            };
+          });
+        in /*bash*/ ''
           HOME=$(mktemp -d)
-          [ ! -f $src/bin/testvim ] && exit 1 || echo "$src/bin/testvim exists!"
-          ${testvim}/bin/testvim --headless --cmd "lua =require('nixCats')"
+          TEST_TEMP=$(mktemp -d)
+          mkdir -p $TEST_TEMP $HOME
+          [ ! -f ${drvtestvim}/bin/testvim ] && exit 1 || echo "${drvtestvim}/bin/testvim exists!"
+          ${drvtestvim}/bin/testvim --headless --cmd "lua vim.g.nix_test_out = [[$out]]; vim.g.nix_test_temp = [[$TEST_TEMP]]"
         '';
       };
     };
