324835ba9e325200e3b153a5e8b17523bdb3a003
BirdeeHub <robindbor@gmail.com>
2024-11-01 07:07:51 -0700

    comment

--- templates/example/lua/myLuaConf/init.lua
+++ templates/example/lua/myLuaConf/init.lua
@@ -4,6 +4,7 @@ require('myLuaConf.opts_and_keys')
 
 -- NOTE: register the extra lze handlers because we want to use them.
 require("lze").register_handlers(require('lze.x'))
+-- NOTE: also add another one that makes enabling a spec for a category nicer
 require("lze").register_handlers(require('nixCatsUtils.lzUtils').for_cat)
 
 -- NOTE: general plugins
