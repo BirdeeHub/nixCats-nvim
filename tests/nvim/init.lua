3760724804b1ace69a49e96b0b089a3c6a6504a8
BirdeeHub <robindbor@gmail.com>
2024-11-01 18:54:30 -0700

    comment

--- tests/nvim/init.lua
+++ tests/nvim/init.lua
@@ -1,6 +1,9 @@
 make_test("hello", function()
     assert.truthy(nixCats("nixCats_test_names.hello"))
 end)
+
+-- NOTE: done this way so config still
+-- gets ran when lua_dir test is not enabled
 local ok, err = pcall(require, "config")
 make_test("lua_dir", function()
     assert.truthy(ok, err)
