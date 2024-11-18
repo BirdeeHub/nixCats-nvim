c0ee76df0de887a8d2e6af16f180165388879bd0
birdee <birdee@birdee.com>
2024-11-18 10:13:24 -0800

    improve error messages for custom lua inline types

--- utils/n2l.nix
+++ utils/n2l.nix
@@ -27,7 +27,7 @@ with builtins; let
     mkBaseT = expr: { __type = id; inherit expr; };
     mkmk = n: p: v: mkBaseT ((p.fields or {}) // { type = n; } // ((p.default or (o: o)) v));
     types = mapAttrs (n: p: p // { name = n; mk = mkmk n p;
-        check = v: (p.check or (_:true)) v && typeof v == n;
+        check = v: typeof v == n && (p.check or (_:true)) v == true;
       }) proto;
     default_subtype = let
       defvals = attrNames (filterAttrs (n: x: isFunction (x.default or false)) proto);
@@ -39,34 +39,43 @@ with builtins; let
       && null != types."${v.expr.type or default_subtype}".name or null;
     typeof = v: if ! (member v) then null
       else types."${v.expr.type or default_subtype}".name;
-    resolve = v: let vt = typeof v; in
+    resolve = v: let vt = typeof v; checkres = proto."${vt}".check v; in
       if vt == null then throw "unable to resolve, not subtype of ${id}"
-      else if types."${vt}".check v then (proto."${vt}".format or (o: o.expr)) v
-      else throw "unable to resolve, value is not a valid instance of type ${id}.${vt}";
+      else if checkres == true then (proto."${vt}".format or (o: o.expr)) v
+      else throw "unable to resolve, value is not a valid instance of type ${id}.${vt}.\nError: ${checkres}";
   in { inherit types typeof member resolve mkBaseT id default_subtype; };
 
   LIproto = let
-    fixargs = args: if any (v: ! isString v || builtins.match ''^([A-Za-z_][A-Za-z0-9_]*|\.\.\.)$'' v == null) args
-      then throw "args must be valid lua identifiers"
-      else concatStringsSep ", " args;
+    inlinecheck = v: if isString (v.expr.body or null)
+      then true else "body attribute must be a string";
+    funccheck = v: if ! isString (v.expr.body or null)
+      then "body attribute must be a string"
+      else if ! isList (v.expr.args or null)
+        || any (val: ! isString val || builtins.match ''^([A-Za-z_][A-Za-z0-9_]*|\.\.\.)$'' val == null) v.expr.args
+      then "args attribute must be a list of strings containing valid lua identifier names"
+      else true;
   in {
     inline-safe = {
       default = (v: if v ? body then v else { body = v; });
       fields = { body = "nil"; };
-      format = LI: "(assert(loadstring(${luaEnclose "return ${LI.expr.body or LI.expr or "nil"}"}))())";
+      check = inlinecheck;
+      format = LI: "(assert(loadstring(${luaEnclose "return ${LI.expr.body or "nil"}"}))())";
     };
     inline-unsafe = {
       fields = { body = "nil"; };
+      check = inlinecheck;
       format = LI: "${LI.expr.body or "nil"}";
     };
     function-safe = {
       fields = { body = "return nil"; args = []; };
+      check = funccheck;
       format = LI:
-        ''(assert(loadstring(${luaEnclose ''return (function(${fixargs (LI.expr.args or [])}) ${LI.expr.body or "return nil"} end)''}))())'';
+        ''(assert(loadstring(${luaEnclose ''return (function(${concatStringsSep ", " (LI.expr.args or [])}) ${LI.expr.body or "return nil"} end)''}))())'';
     };
     function-unsafe = {
       fields = { body = "return nil"; args = []; };
-      format = LI: ''(function(${fixargs (LI.expr.args or [])}) ${LI.expr.body or "return nil"} end)'';
+      check = funccheck;
+      format = LI: ''(function(${concatStringsSep ", " (LI.expr.args or [])}) ${LI.expr.body or "return nil"} end)'';
     };
     with-meta = {
       fields = {
@@ -75,6 +84,14 @@ with builtins; let
         newtable = null; # <- if you want to specify a different first arg to setmetatable
         tablevar = "tbl_in"; # <- varname to refer to the table, to avoid translating multiple times
       };
+      check = v:
+        if ! isAttrs (v.expr.table or null) || ! isAttrs (v.expr.meta or null) then 
+          "both 'table' and 'meta' attributes are required and must be tables"
+        else if ! isString (v.expr.tablevar or null)
+          || builtins.match ''^([A-Za-z_][A-Za-z0-9_]*|\.\.\.)$'' v.expr.tablevar == null then 
+          "'tablevar' must be a valid lua identifier name"
+        else 
+          true;
       format = LI: opts: let
         metaarg1 = if LI.expr.newtable or null == null then LI.expr.tablevar or "{}" else toLuaFull opts LI.expr.newtable;
         result = inline.types.function-unsafe.mk {
