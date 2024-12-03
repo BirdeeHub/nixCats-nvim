e14a2a1130c03f9c0229f8af1a5df8df780a592e
BirdeeHub <birdee@localhost>
2024-12-03 07:57:23 -0800

    tests

--- tests/nixos/main.nix
+++ tests/nixos/main.nix
@@ -1,5 +1,19 @@
-{ config, pkgs, package, utils, lib, inputs, packagename, ...  }@args: {
-  ${packagename} = {
+{ config, pkgs, package, utils, lib, inputs, packagename, moduleNamespace, ...  }@args:
+lib.setAttrByPath moduleNamespace {
+  enable = true;
+  packageNames = [ packagename ];
+  packageDefinitions.replace = {
+    ${packagename} = { pkgs, ... }: {
+      settings = {
+      };
+      categories = {
+        nixCats_test_names = {
+          nixos_hello = true;
+        };
+      };
+    };
+  };
+  users.testuser = {
     enable = true;
     packageNames = [ packagename ];
     packageDefinitions.replace = {
@@ -8,22 +22,7 @@
         };
         categories = {
           nixCats_test_names = {
-            nixos_hello = true;
-          };
-        };
-      };
-    };
-    users.testuser = {
-      enable = true;
-      packageNames = [ packagename ];
-      packageDefinitions.replace = {
-        ${packagename} = { pkgs, ... }: {
-          settings = {
-          };
-          categories = {
-            nixCats_test_names = {
-              nixos_user_hello = true;
-            };
+            nixos_user_hello = true;
           };
         };
       };
