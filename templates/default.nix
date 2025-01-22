f3b40d919a2a0f5a75f317e3b689f13f1e0a05e0
quarkQuark <54946378+quarkQuark@users.noreply.github.com>
2025-01-22 11:46:44 +0000

    clarify template docs (#86)

--- templates/default.nix
+++ templates/default.nix
@@ -1,24 +1,29 @@
 {
   default = {
     path = ./fresh;
-    description = "Starting point template for making your Neovim flake";
+    description = ''
+      Starting point template for making your Neovim flake.
+      This is the same as the `fresh` flake template.
+    '';
   };
   fresh = {
     path = ./fresh;
-    description = "Starting point template for making your Neovim flake";
+    description = ''
+      Starting point template for making your Neovim flake.
+    '';
   };
   home-manager = {
     path = ./home-manager;
     description = ''
-      demonstration of importing and using the nixCats module for Home-Manager
+      Demonstration of importing and using the nixCats module for Home Manager.
     '';
   };
   nixos = {
     path = ./nixos;
     description = ''
-      demonstration of importing and using the nixCats module for NixOS (and nix-darwin)
+      Demonstration of importing and using the nixCats module for NixOS (and nix-darwin).
 
-      same as home manager, but has the options repeated for per-user configurations
+      Same as the `home-manager` template, but has the options repeated for per-user configurations.
     '';
   };
   nixExpressionFlakeOutputs = {
@@ -29,28 +34,35 @@
 
       It is best practice to avoid using the system pkgs and its overlays in this method,
       as then you could not output packages for systems not defined in your system flake.
-      It creates a new one instead to use, just like the flake template does.
+      It creates a new one instead to use, just like the `fresh` flake template does.
 
       Call it from your system flake and call it with inputs as arguments.
     '';
   };
   example = {
     path = ./example;
-    description = "an idiomatic nixCats example configuration using lze for lazy loading and paq.nvim for backup when not using nix";
+    description = ''
+      An idiomatic nixCats example configuration using
+      lze for lazy loading and paq.nvim for backup when not using nixCats-nvim.
+
+      See also https://github.com/BirdeeHub/nixCats-nvim/tree/main/templates/example#README.md.
+      '';
   };
   kickstart-nvim = {
     path = ./kickstart-nvim;
     description = ''
       The entirety of kickstart.nvim implemented as a nixCats flake.
-      With additional Nix LSPs for editing the nix part.
+      With additional Nix LSPs for editing the Nix part.
       This is to serve as the tutorial for using the nixCats lazy wrapper.
+
+      See also https://github.com/BirdeeHub/nixCats-nvim/tree/main/templates/kickstart-nvim#README.md.
     '';
   };
   LazyVim = {
     path = ./LazyVim;
     description = ''
-      How to get the LazyVim distribution up and running
-      see the kickstart-nvim template for more info on the lazy wrapper or other utilities used.
+      How to get the LazyVim distribution up and running.
+      See the `kickstart-nvim` template for more info on the lazy wrapper or other utilities used.
     '';
   };
   overwrite = {
@@ -61,17 +73,17 @@
       achieved via the OVERRIDE function.
 
       Equivalent to the default flake template
-      or nixExpressionFlakeOutputs except
-      for using overrides
+      or `nixExpressionFlakeOutputs` except
+      for using overrides.
 
-      every nixCats package is a full nixCats-nvim
+      Every nixCats package is a full nixCats-nvim.
     '';
   };
   luaUtils = {
     path = ./luaUtils;
     description = ''
-      A template that includes lua utils for using neovim package managers
-      when your config file is not loaded via nix.
+      A template that includes Lua utils for using Neovim package managers
+      when your config file is not loaded via Nix.
     '';
   };
   overriding = {
@@ -90,15 +102,15 @@
   overlayHub = {
     path = ./overlayHub;
     description = ''
-      A template for overlays/default.nix
-      :help nixCats.flake.nixperts.overlays
+      A template for overlays/default.nix.
+      See `:help nixCats.flake.nixperts.overlays`.
     '';
   };
   overlayFile = {
     path = ./overlayfile;
     description = ''
       A template for an empty overlay file defined as described in
-      :help nixCats.flake.nixperts.overlays
+      `:help nixCats.flake.nixperts.overlays`.
     '';
   };
 }
