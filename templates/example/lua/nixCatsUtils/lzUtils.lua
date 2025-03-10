ad8d22d086cfb7a1cb0e9fda1fb871bbe370c942
BirdeeHub <birdee@localhost>
2025-03-10 05:11:00 -0700

    fix(nixCatsUtils.lzUtils.for_cat): Wow... How... Nevermind... just copy the new one if you used it
    addresses https://github.com/BirdeeHub/nixCats-nvim/issues/109

--- templates/example/lua/nixCatsUtils/lzUtils.lua
+++ templates/example/lua/nixCatsUtils/lzUtils.lua
@@ -20,17 +20,13 @@ M.for_cat = {
     spec_field = "for_cat",
     set_lazy = false,
     modify = function(plugin)
-        if type(plugin.for_cat) == "table" then
-            if plugin.for_cat.cat ~= nil then
-                if vim.g[ [[nixCats-special-rtp-entry-nixCats]] ] ~= nil then
-                    plugin.enabled = (nixCats(plugin.for_cat.cat) and true) or false
-                else
-                    plugin.enabled = nixCats(plugin.for_cat.default)
-                end
+        if type(plugin.for_cat) == "table" and plugin.for_cat.cat ~= nil then
+            if vim.g[ [[nixCats-special-rtp-entry-nixCats]] ] ~= nil then
+                plugin.enabled = (nixCats(plugin.for_cat.cat) and true) or false
             else
-                plugin.enabled = (nixCats(plugin.for_cat) and true) or false
+                plugin.enabled = plugin.for_cat.default
             end
-        elseif type(plugin.for_cat) == "string" then
+        else
             plugin.enabled = (nixCats(plugin.for_cat) and true) or false
         end
         return plugin
