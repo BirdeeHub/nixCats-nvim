10c4445715d6b3fdb680b972bcf709c8f9ef254c
BirdeeHub <birdee@localhost>
2025-03-17 03:00:16 -0700

    tests(templates): more template checks in tests

--- tests/flake.nix
+++ tests/flake.nix
@@ -11,19 +11,30 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-appimage.url = "github:ralismark/nix-appimage";
   };
   outputs = { self, nixpkgs, ... }@inputs: let
     utils = import ../.;
     forAllSys = utils.eachSystem nixpkgs.lib.platforms.all;
     packagename = "testvim";
     stateVersion = "24.05";
+    nixCats = (let
+      nixosModule = utils.mkNixosModules {};
+      homeModule = utils.mkHomeModules {};
+    in {
+      outPath = ../.;
+      inherit utils nixosModule homeModule;
+      inherit (utils) templates;
+      nixosModules.default = nixosModule;
+      homeModules.default = homeModule;
+    });
   in forAllSys (system: let
     pkgs = import nixpkgs { inherit system; };
     libT = pkgs.callPackage ./libT { inherit inputs utils; };
 
     package = import ./nvim { inherit inputs utils system packagename; };
 
-    pureCallFlake = path: let
+    pureCallFlake = nixCats: path: let
       bareflake = import "${path}/flake.nix";
       res = bareflake.outputs (inputs // rec {
         self = res // {
@@ -33,21 +44,17 @@
               inputs.${n} or { inherit nixCats self; }.${n} or builtins.throw "Missing input ${n}"
             ) bareflake.inputs;
         };
-        nixCats = (let
-          nixosModule = utils.mkNixosModules {};
-          homeModule = utils.mkHomeModules {};
-        in {
-          outPath = ../.;
-          inherit utils nixosModule homeModule;
-          inherit (utils) templates;
-          nixosModules.default = nixosModule;
-          homeModules.default = homeModule;
-        });
+        inherit nixCats;
       });
     in res;
 
-    exampleconfig = (pureCallFlake utils.templates.example.path).packages.${system}.default;
-    kickstartconfig = (pureCallFlake utils.templates.kickstart-nvim.path).packages.${system}.default;
+    exampleflake = (pureCallFlake nixCats utils.templates.example.path);
+    exampleconfig = exampleflake.packages.${system}.default;
+    kickstartconfig = (pureCallFlake nixCats utils.templates.kickstart-nvim.path).packages.${system}.default;
+    overriding = (pureCallFlake exampleflake utils.templates.overriding.path).packages.${system}.default;
+    overwrite = (pureCallFlake exampleflake utils.templates.overwrite.path).packages.${system}.default;
+    LazyVim = (pureCallFlake nixCats utils.templates.LazyVim.path).packages.${system}.default;
+    flakeless = import ../templates/flakeless { inherit pkgs nixCats; };
 
     testargs2 = {
       inherit inputs utils libT stateVersion;
@@ -71,6 +78,8 @@
     checks = {
       inherit drvtests hometests nixostests;
       inherit exampledrvtests kickstartdrvtests;
+      # sanity template build checks
+      inherit overwrite overriding LazyVim flakeless;
     };
   });
 }
