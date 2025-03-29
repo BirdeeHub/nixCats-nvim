0290b12aff24349c29c31c2a3868b9cbf2ea8bd2
BirdeeHub <birdee@localhost>
2025-03-29 15:04:43 -0700

    docs(templates): lspconfig stopped accepting vim.defaulttable for some reason (it loops forever when loaded...)

--- templates/simple/plugin/lsp.lua
+++ templates/simple/plugin/lsp.lua
@@ -5,57 +5,69 @@ end
 
 -- vim.defaulttable() will allow us to set values in the `servers` table
 -- like a nix attribute set without explicitly creating each internal table.
-local servers = vim.defaulttable()
+local servers = {}
 
 -- most don't need much configuration
 -- servers.gopls = {}
 -- servers.html = {}
 
 -- but you can provide some if you want to!
-servers.lua_ls.settings.Lua = {
-  formatters = {
-    ignoreComments = true,
-  },
-  signatureHelp = { enabled = true },
-  diagnostics = {
-    globals = { 'vim', 'nixCats' },
-    disable = { 'missing-fields' },
-  },
+servers = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        formatters = {
+          ignoreComments = true,
+        },
+        signatureHelp = { enabled = true },
+        diagnostics = {
+          globals = { 'vim', 'nixCats' },
+          disable = { 'missing-fields' },
+        },
+      }
+    }
+  }
 }
 -- nixd requires some configuration.
 -- luckily, the nixCats plugin is here to pass whatever we need!
 -- for additional configuration options, refer to:
 -- https://github.com/nix-community/nixd/blob/main/nixd/docs/configuration.md
-servers.nixd.settings.nixd = {
-  nixpkgs = {
-    -- in the extras set of your package definition:
-    -- nixdExtras.nixpkgs = ''import ${pkgs.path} {}''
-    expr = nixCats.extra("nixdExtras.nixpkgs") or [[import <nixpkgs> {}]],
-  },
-  options = {
-    -- If you integrated with your system flake,
-    -- you should use inputs.self as the path to your system flake
-    -- that way it will ALWAYS work, regardless
-    -- of where your config actually was.
-    nixos = {
-      -- in this package definition's.extra set
-      -- nixdExtras.nixos_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.configname.options''
-      expr = nixCats.extra("nixdExtras.nixos_options")
-    },
-    -- If you have your config as a separate flake, inputs.self would be referring to the wrong flake.
-    -- You can override the correct one into your package definition on import in your main configuration,
-    -- or just put an absolute path to where it usually is and accept the impurity.
-    ["home-manager"] = {
-      -- nixdExtras.home_manager_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").homeConfigurations.configname.options''
-      expr = nixCats.extra("nixdExtras.home_manager_options")
-    }
-  },
-  formatting = {
-    command = { "nixfmt" }
-  },
-  diagnostic = {
-    suppress = {
-      "sema-escaping-with"
+servers = {
+  nixd = {
+    settings = {
+      nixd = {
+        nixpkgs = {
+          -- in the extras set of your package definition:
+          -- nixdExtras.nixpkgs = ''import ${pkgs.path} {}''
+          expr = nixCats.extra("nixdExtras.nixpkgs") or [[import <nixpkgs> {}]],
+        },
+        options = {
+          -- If you integrated with your system flake,
+          -- you should use inputs.self as the path to your system flake
+          -- that way it will ALWAYS work, regardless
+          -- of where your config actually was.
+          nixos = {
+            -- in this package definition's.extra set
+            -- nixdExtras.nixos_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.configname.options''
+            expr = nixCats.extra("nixdExtras.nixos_options")
+          },
+          -- If you have your config as a separate flake, inputs.self would be referring to the wrong flake.
+          -- You can override the correct one into your package definition on import in your main configuration,
+          -- or just put an absolute path to where it usually is and accept the impurity.
+          ["home-manager"] = {
+            -- nixdExtras.home_manager_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").homeConfigurations.configname.options''
+            expr = nixCats.extra("nixdExtras.home_manager_options")
+          }
+        },
+        formatting = {
+          command = { "nixfmt" }
+        },
+        diagnostic = {
+          suppress = {
+            "sema-escaping-with"
+          }
+        }
+      }
     }
   }
 }
@@ -101,7 +113,6 @@ lspconfig.util.default_config = vim.tbl_extend(
       vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         vim.lsp.buf.format()
       end, { desc = 'Format current buffer with LSP' })
-
     end,
   }
 )
