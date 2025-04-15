3867fbc32e446b5254583fb495b2f3dd5057a842
BirdeeHub <birdee@localhost>
2025-04-15 14:03:05 -0700

    refactor(user_command): separated a function

--- builder/nixCats/debug.lua
+++ builder/nixCats/debug.lua
@@ -10,83 +10,88 @@ local attributes = {
     "nixCatsPath",
     "packageBinPath",
 }
-function M.command(opts)
-    local function basic_lua_popup(input)
-        local function mk_popup(text)
-            local contents = {}
-            for line in text:gmatch("[^\r\n]+") do
-                table.insert(contents, line)
-            end
-            local bufnr = vim.api.nvim_create_buf(false, true)
-            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
-            vim.bo[bufnr].modifiable = false
-            vim.bo[bufnr].readonly = true
-            vim.bo[bufnr].filetype = "lua"
-
-            -- Get maximum width of text
-            local width = 0
-            for _, line in ipairs(contents) do
-                width = math.max(width, #line)
-            end
-
-            -- cap to screen size with margin
-            local height = #contents
-            local win_width = math.min(width + 2, vim.o.columns - 4)
-            local win_height = math.min(height + 2, vim.o.lines - 4)
-            local popopts = {
-                relative = "editor",
-                width = win_width,
-                height = win_height,
-                row = (vim.o.lines - win_height) / 2,
-                col = (vim.o.columns - win_width) / 2,
-                style = "minimal",
-                border = "rounded",
-            }
-
-            -- make the window
-            local win_id = vim.api.nvim_open_win(bufnr, true, popopts)
-            vim.wo[win_id].signcolumn = "no"
-            vim.wo[win_id].number = false
-            vim.wo[win_id].relativenumber = false
-
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<Cmd>close<CR>", { noremap = true, silent = true })
-            vim.api.nvim_buf_set_keymap(bufnr, "n", "<Esc>", "<Cmd>close<CR>", { noremap = true, silent = true })
-
-            vim.api.nvim_create_autocmd("BufLeave", {
-                buffer = bufnr,
-                once = true,
-                callback = function()
-                    vim.api.nvim_win_close(win_id, true)
-                end,
-            })
+local function basic_lua_popup(input)
+    local function mk_popup(text)
+        local contents = {}
+        for line in text:gmatch("[^\r\n]+") do
+            table.insert(contents, line)
         end
-        local ok, msg = pcall(mk_popup, input)
-        if not ok then
-            print("Popup failed to open due to error: " .. msg)
-            print("Falling back to print()")
-            print(input)
+        local bufnr = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+        vim.bo[bufnr].modifiable = false
+        vim.bo[bufnr].readonly = true
+        vim.bo[bufnr].filetype = "lua"
+
+        -- Get maximum width of text
+        local width = 0
+        for _, line in ipairs(contents) do
+            width = math.max(width, #line)
         end
+
+        -- cap to screen size with margin
+        local height = #contents
+        local win_width = math.min(width + 2, vim.o.columns - 4)
+        local win_height = math.min(height + 2, vim.o.lines - 4)
+        local popopts = {
+            relative = "editor",
+            width = win_width,
+            height = win_height,
+            row = (vim.o.lines - win_height) / 2,
+            col = (vim.o.columns - win_width) / 2,
+            style = "minimal",
+            border = "rounded",
+        }
+
+        -- make the window
+        local win_id = vim.api.nvim_open_win(bufnr, true, popopts)
+        vim.wo[win_id].signcolumn = "no"
+        vim.wo[win_id].number = false
+        vim.wo[win_id].relativenumber = false
+
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<Cmd>close<CR>", { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "<Esc>", "<Cmd>close<CR>", { noremap = true, silent = true })
+
+        vim.api.nvim_create_autocmd("BufLeave", {
+            buffer = bufnr,
+            once = true,
+            callback = function()
+                vim.api.nvim_win_close(win_id, true)
+            end,
+        })
     end
+    local ok, msg = pcall(mk_popup, input)
+    if not ok then
+        print("Popup failed to open due to error: " .. msg)
+        print("Falling back to print()")
+        print(input)
+    end
+end
 
-    local display = vim.g.nixcats_debug_ui ~= false and basic_lua_popup or print
-
+function M.display(input)
+    if vim.g.nixcats_debug_ui ~= false then
+        basic_lua_popup(input)
+    else
+        print(input)
+    end
+end
+function M.command(opts)
     if #opts.fargs == 0 then
-        display(vim.inspect(nixCats.cats))
+        M.display(vim.inspect(nixCats.cats))
         return
     elseif #opts.fargs == 1 then
         if vim.list_contains(attributes, opts.fargs[1]) then
-            display(vim.inspect(nixCats[opts.fargs[1]]))
+            M.display(vim.inspect(nixCats[opts.fargs[1]]))
             return
         end
     elseif #opts.fargs == 2 then
         if opts.fargs[1] == 'cat' or opts.fargs[1] == 'get' then
-            display(vim.inspect(nixCats.get(opts.fargs[2])))
+            M.display(vim.inspect(nixCats.get(opts.fargs[2])))
             return
         end
     elseif #opts.fargs > 2 then
         local first = table.remove(opts.fargs, 1)
         if first == 'cat' or first == 'get' then
-            display(vim.inspect(nixCats.get(opts.fargs)))
+            M.display(vim.inspect(nixCats.get(opts.fargs)))
             return
         end
     end
