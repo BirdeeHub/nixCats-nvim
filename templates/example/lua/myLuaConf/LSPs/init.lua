ea484dd120d3a0da01599d591c58f270bfbb2643
BirdeeHub <birdee@localhost>
2025-04-19 18:04:51 -0700

    docs(templates): more cleanup to example template lsp setup

--- templates/example/lua/myLuaConf/LSPs/init.lua
+++ templates/example/lua/myLuaConf/LSPs/init.lua
@@ -2,17 +2,18 @@ local catUtils = require('nixCatsUtils')
 if (catUtils.isNixCats and nixCats('lspDebugMode')) then
   vim.lsp.set_log_level("debug")
 end
+local old_ft_fallback = require('lze').h.lsp.get_ft_fallback()
 require('lze').h.lsp.set_ft_fallback(function(name)
   local lspcfg = nixCats.pawsible({ "allPlugins", "opt", "nvim-lspconfig" }) or nixCats.pawsible({ "allPlugins", "start", "nvim-lspconfig" })
-  if not lspcfg then
-    local matches = vim.api.nvim_get_runtime_file("pack/*/{start,opt}/nvim-lspconfig", false)
-    lspcfg = assert(matches[1], "nvim-lspconfig not found!")
+  if lspcfg then
+    local ok, cfg = pcall(dofile, lspcfg .. "/lsp/" .. name .. ".lua")
+    if not ok then
+      ok, cfg = pcall(dofile, lspcfg .. "/lua/lspconfig/configs/" .. name .. ".lua")
+    end
+    return (ok and cfg or {}).filetypes or {}
+  else
+    return old_ft_fallback(name)
   end
-  local ok, cfg = pcall(dofile, lspcfg .. "/lsp/" .. name .. ".lua")
-  if not ok then
-    ok, cfg = pcall(dofile, lspcfg .. "/lua/lspconfig/configs/" .. name .. ".lua")
-  end
-  return (ok and cfg or {}).filetypes or {}
 end)
 -- file uses lzextras.lsp handler
 require('lze').load {
@@ -115,7 +116,6 @@ require('lze').load {
     "nixd",
     enabled = catUtils.isNixCats and (nixCats('nix') or nixCats('neonixdev')) or false,
     lsp = {
-      filetypes = { 'nix' },
       settings = {
         nixd = {
           -- nixd requires some configuration.
