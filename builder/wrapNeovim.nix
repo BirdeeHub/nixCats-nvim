e94f31ad8a49fb6f7c9b87cbaf83f478d929cf28
BirdeeHub <birdee@localhost>
2025-04-21 16:23:44 -0700

    refactor(builder): moved a ) down a line

--- builder/wrapNeovim.nix
+++ builder/wrapNeovim.nix
@@ -36,8 +36,8 @@ let
 
   normSpecs = control: var: maker: lib.flip lib.pipe [
     (lib.partition (p: nclib.ncIsAttrs p && p ? value && (
-      (p.pre or false == true && control == "--suffix") || (p.pre or true == false && control == "--prefix")))
-    )
+      (p.pre or false == true && control == "--suffix") || (p.pre or true == false && control == "--prefix"))
+    ))
     ({ right ? [], wrong ? []}: {
       pre = map (p: if nclib.ncIsAttrs p then p.value or p else p) (if control == "--suffix" then right else wrong);
       post = map (p: if nclib.ncIsAttrs p then p.value or p else p) (if control == "--prefix" then right else wrong);
